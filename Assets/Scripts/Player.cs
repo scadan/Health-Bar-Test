9ecdba66fcc9eb758b20f2adb9fcba2d9749f2ea
scadan <aidan.yates.7@gmail.com>
2018-10-10 11:50:52 +0100

    I have made a simple health bar in Unity
    I have made this to hopefully use it in future projects

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private Stat health;
+
+
+	// Use this for initialization
+	void Start () {
+
+        
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Stat.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stat
+{
+    private BarScript bar;
+
+    private float maxVal;
+
+    private float currentVal;
+
+    public float CurrentVal
+    {
+        get
+        {
+            return currentVal;
+        }
+
+        set
+        {
+            
+            this.currentVal = value;
+            bar.Value = currentVal;
+
+        }
+    }
+
+    public float MaxVal
+    {
+        get
+        {
+            return maxVal;
+        }
+
+        set
+        {
+            this.maxVal = value;
+            bar.MaxValue = maxVal;
+            
+        }
+    }
+}
